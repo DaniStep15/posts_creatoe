1b7d5665245c3ea6c01b79ae795325ab347ad671
Danil Stepanov <daniladan689@gmail.com>
2021-06-09 23:17:22 +0300

    get gata from DB to UI

--- src/components/posts.component.js
+++ src/components/posts.component.js
@@ -3,13 +3,53 @@ import { apiService } from '../services/api.service'
 import { TransformService } from '../services/transform.service'
 
 export class PostsComponent extends Component {
-  constructor(id) {
+  constructor(id, { loader }) {
     super(id)
+    this.loader = loader
   }
 
   async onShow() {
+    this.loader.show()
     const fbData = await apiService.fetchPosts()
     const posts = TransformService.fbObjectToArray(fbData)
-    console.log(posts)
+    this.loader.hide()
+
+    const html = posts.map((post) => renderPost(post)).join(' ')
+
+    this.$el.insertAdjacentHTML('afterbegin', html)
   }
+
+  onHide() {
+    this.loader.hide()
+    this.$el.innerHTML = ''
+  }
+}
+
+function renderPost(post) {
+  const tag =
+    post.type === 'news'
+      ? '<li class="tag tag-blue tag-rounded">News</li>'
+      : '<li class="tag  tag-rounded">Post</li>'
+
+  const button =
+    '<button class="button-round button-small button-primary">Save</button>'
+
+  return `
+        <div class="panel">
+        <div class="panel-head">
+          <p class="panel-title">${post.title}</p>
+          <ul class="tags">
+          ${tag}
+          </ul>
+        </div>
+        <div class="panel-body">
+          <p class="multi-line">${post.fulltext}</p>
+        </div>
+        <div class="panel-footer w-panel-footer">
+          <small>${post.date}</small>
+          ${button}
+        </div>
+        </div>
+
+  `
 }
